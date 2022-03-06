199bba6f8ea8e917fb15398d842bc5866a5206b2
Zachary Chavez <zpchavez@gmail.com>
2022-03-05 21:56:04 -0700

    Simplify wizard. No more copying TF files to patch directory

--- Wizard/Program.cs
+++ Wizard/Program.cs
@@ -32,63 +32,44 @@ namespace Wizard
     [STAThread]
     static void Main()
     {
-      bool gui = Environment.GetCommandLineArgs().Count() == 1;
+      bool noArgs = Environment.GetCommandLineArgs().Count() == 1;
       try {
         Environment.CurrentDirectory = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
 
-        string destPath;
-        if (gui) {
+        string destPath = "";
+        if (noArgs) {
           Application.EnableVisualStyles();
           Application.SetCompatibleTextRenderingDefault(false);
 
           destPath = (Environment.GetEnvironmentVariable("ProgramFiles(x86)") ?? "") + @"\Steam\SteamApps\common\TowerFall";
-          if (!IsProbablyTowerFallDir(destPath))
+          if (!IsProbablyTowerFallDir(destPath)) {
             destPath = (Environment.GetEnvironmentVariable("HOME") ?? "") + "/.steam/steam/SteamApps/common/TowerFall";
-          while (!IsProbablyTowerFallDir(destPath)) {
-            using (var dialog = new FolderBrowserDialog { Description = @"Please select the SteamApps\common\TowerFall directory." }) {
-              if (dialog.ShowDialog() != DialogResult.OK)
-                return;
-              destPath = dialog.SelectedPath;
-            }
+          }
+
+          if (!IsProbablyTowerFallDir(destPath)) {
+            Console.WriteLine("Could not locate TowerFall directory. Try specifying the path as an argument.");
+            return;
           }
         } else {
           destPath = Environment.GetCommandLineArgs()[1];
         }
 
-        Directory.CreateDirectory(Path.Combine("Original", "Content", "Atlas"));
-        foreach (string file in RelativeEnumerateFiles(Path.Combine(destPath))) {
-          TryCopy(Path.Combine(destPath, file), Path.Combine(file));
+        // If backup exists, restore the original TowerFall.exe before patching
+        if (File.Exists(Path.Combine(destPath, "TowerFall-Original.exe"))) {
+          File.Copy(Path.Combine(destPath, "TowerFall-Original.exe"), Path.Combine(destPath, "TowerFall.exe"), overwrite: true);
+        } else {
+          File.Copy(Path.Combine(destPath, "TowerFall.exe"), Path.Combine(destPath, "TowerFall-Original.exe"), overwrite: true);
         }
-        TryCopy(Path.Combine(destPath, "TowerFall.exe"), Path.Combine("Original", "TowerFall.exe"));
-        File.Copy(Path.Combine("Original", "TowerFall.exe"), "TowerFall.exe", overwrite: true);
-        Patcher.Patcher.Patch("Mod.dll");
 
-        foreach (string file in RelativeEnumerateFiles(Path.Combine(destPath, "Content", "Atlas"))) {
-          TryCopy(Path.Combine(destPath, "Content", "Atlas", file), Path.Combine("Original", "Content", "Atlas", file));
-        }
-        Patcher.Patcher.PatchResources();
+        Patcher.Patcher.Patch("Mod.dll", destPath, Path.Combine(destPath, "TowerFall.exe"));
 
-        File.Copy(Path.Combine("TowerFall.exe"), Path.Combine(destPath, "TowerFall.exe"), overwrite: true);
         File.Copy(Path.Combine("Mod.dll"), Path.Combine(destPath, "Mod.dll"), overwrite: true);
-        File.Copy(Path.Combine("Content", "Atlas", "modAtlas.xml"), Path.Combine(destPath, "Content", "Atlas", "modAtlas.xml"));
-        File.Copy(Path.Combine("Content", "Atlas", "modAtlas.png"), Path.Combine(destPath, "Content", "Atlas", "modAtlas.png"));
-        foreach (string file in RelativeEnumerateFiles(Path.Combine(destPath, "Content", "Atlas"))) {
-          if (File.Exists(Path.Combine("Content", "Atlas", file))) {
-            File.Copy(Path.Combine("Content", "Atlas", file), Path.Combine(destPath, "Content", "Atlas", file), overwrite: true);
-          }
-        }
+        File.Copy(Path.Combine("modAtlas.xml"), Path.Combine(destPath, "Content", "Atlas", "modAtlas.xml"), overwrite: true);
+        File.Copy(Path.Combine("modAtlas.png"), Path.Combine(destPath, "Content", "Atlas", "modAtlas.png"), overwrite: true);
 
-        if (gui) {
-          MessageBox.Show("Success!");
-        } else {
-          Console.WriteLine("Success!");
-        }
+        Console.WriteLine("Success!");
       } catch (Exception e) {
-        if (gui) {
-          MessageBox.Show("Error:" + Environment.NewLine + e.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-        } else {
-          throw;
-        }
+        throw e;
       }
     }
   }
