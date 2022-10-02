b320e02e1125d5b153057ff98737e5341f7acfc3
Zachary Chavez <zpchavez@gmail.com>
2022-10-02 12:04:53 -0700

    Update stats posting to use MonoMod

--- Mod/Classes/Patched/VersusMatchResults.cs
+++ Mod/Classes/Patched/VersusMatchResults.cs
@@ -1,31 +1,33 @@
-using Patcher;
-using TowerFall;
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json.Linq;
+using Mod;
 
-namespace Mod
+namespace TowerFall
 {
-  [Patch]
-  public class MyVersusMatchResults : VersusMatchResults
+  public class patch_VersusMatchResults : VersusMatchResults
   {
 
-    public MyVersusMatchResults (Session session, VersusRoundResults roundResults) : base(session, roundResults)
+    public patch_VersusMatchResults (Session session, VersusRoundResults roundResults) : base(session, roundResults)
     {
+      // no-op
     }
 
     #if (STAT_TRACKING)
-      public override void Added ()
+      public extern void orig_Added();
+      public void patch_Added ()
       {
-        base.Added();
+        orig_Added();
 
         TrackerApiClient client = new TrackerApiClient();
 
         if (client.IsSetup()) {
           JObject stats = new JObject();
-          stats["rounds"] = ((MySession)this.session).RoundsPlayedThisMatch;
+          stats["rounds"] = ((patch_Session)this.session).RoundsPlayedThisMatch;
           stats["kills"] = new JObject();
           stats["deaths"] = new JObject();
           stats["wins"] = new JObject();
@@ -50,7 +52,7 @@ namespace Mod
               stats["revives"][color] = (int)this.session.MatchStats[index].Revives;
               stats["kills_as_ghost"][color] = (int)this.session.MatchStats[index].KillsAsGhost;
               stats["ghost_kills"][color] = (int)this.session.MatchStats[index].GhostKills;
-              stats["miracles"][color] = (int)((MySession)(this.session)).MyMatchStats[index].MiracleCatches;
+              stats["miracles"][color] = (int)((patch_Session)(this.session)).MyMatchStats[index].MiracleCatches;
             }
           }
 
