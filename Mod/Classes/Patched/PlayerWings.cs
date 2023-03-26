d025f1cc70df6b169f113e970d28a9a2ff698111
Zachary Chavez <zpchavez@gmail.com>
2023-03-25 21:28:39 -0700

    Fix wings position in reverse grav

--- Mod/Classes/Patched/PlayerWings.cs
+++ Mod/Classes/Patched/PlayerWings.cs
@@ -20,18 +20,24 @@ namespace TowerFall
       FlipSprite();
     }
 
+    // Update position when player ducks
+    public void patch_PlayerUpdate (float yPos)
+    {
+      this.sprite.Y = patch_Level.IsAntiGrav() ? yPos * -1 : yPos;
+    }
+
     public void FlipSprite()
     {
       bool isRotated = this.sprite.FlipY;
       bool isAntiGrav = patch_Level.IsAntiGrav();
       if (!isRotated && isAntiGrav) {
         this.sprite.FlipY = true;
-        this.sprite.Origin.Y -= 4;
-        this.sprite.Origin.X -= 2f;
+        this.sprite.Origin.Y -= 2;
+        this.sprite.Origin.X += 1f;
       } else if (isRotated && !isAntiGrav) {
         this.sprite.FlipY = false;
-        this.sprite.Origin.Y += 4;
-        this.sprite.Origin.X += 2f;
+        this.sprite.Origin.Y += 2;
+        this.sprite.Origin.X -= 1f;
       }
     }
   }
